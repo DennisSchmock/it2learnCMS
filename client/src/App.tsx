5af27c5baaa335b8ad2967ea78d292d319f56ed8
Dennis Schmock <dennis@schmock.eu>
2021-03-02 09:21:29 +0100

    setup basic node server for backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
 const App = ({ title }: { title: string }): JSX.Element => (
   <div>{title} and then some</div>
 )
 
-App.propTypes = {
-  title: PropTypes.string.isRequired,
-}
 export default App
